d906e612621903c84473f19feb3e7ad9fe046a94
Grzegorz Lasak <gla@mindworking.dk>
2022-09-07 09:47:21 +0200

    Use controller() on routes for grouping

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => 'auth',
-], function($router) {
+], function() {
 
-    Route::post('login', [AuthController::class, 'login']);
+    Route::controller(AuthController::class)->group( function() {
+        Route::post('login', 'login');
 
-    Route::group([
-        'middleware' => 'auth.jwt',
-        'role:user'
-    ], function( $router ) {
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::post('me', [AuthController::class, 'me']);
+        Route::group([
+            'middleware' => 'auth.jwt',
+            'role:user'
+        ], function( $router ) {
+            Route::post('logout', 'logout');
+            Route::post('refresh', 'refresh');
+            Route::post('me', 'me');
+        });
     });
+
 });
